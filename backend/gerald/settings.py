9b9c534bbee1be23964785cbcf990054e0a23bcc
TerrapinStat10n <75049442+TerrapinStat10n@users.noreply.github.com>
2021-05-03 17:30:00 -0400

    django-heroku added

--- backend/gerald/settings.py
+++ backend/gerald/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -154,3 +155,4 @@ CORS_ORIGIN_ALLOW_ALL = True
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 SITE_ID = 1
+django_heroku.settings(locals())
